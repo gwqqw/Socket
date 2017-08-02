a68b1f2deb0fd3ab06698c8a2aa419cc725ae8a0
Andy Gong <gwqqw@163.com>
2017-08-02 21:27:28 +0800

    read and write file

--- Client/src/MySocket/Client.java
+++ Client/src/MySocket/Client.java
@@ -1,10 +1,38 @@
 package MySocket;
 
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+class Handler implements Runnable{
+    Socket socket;
+    OutputStream out;
+    InputStream in;
+
+    Handler(){
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(2055));
+        out = socket.getOutputStream();
+        in = socket.getInputStream();
+    }
+
+    public void Run(){
+        
+    }
+
+}
 
 public class Client {
     public static void main(String [] args){
-        int i = 0;
+
+        try{
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(2055));
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+        }
 
     }
 }
--- src/MySocket/Server.java
+++ src/MySocket/Server.java
@@ -1,27 +1,91 @@
 package MySocket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 class Handler implements Runnable {
     private Socket socket;
-
+    private InputStream in;
+    private OutputStream out;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
     public Handler(Socket socket) {
-        this.socket = socket;
+        try {
+            this.socket = socket;
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+            ois = new ObjectInputStream(in);
+            oos = new ObjectOutputStream(out);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Handler constructor throws exception.");
+        }
+    }
+
+    private void DownloadFile()
+    {
+        DataInputStream din = null;
+        FileOutputStream fos = null;
+        try{
+            System.out.println("Start to download file...");
+            din = new DataInputStream(in);
+            fos = new FileOutputStream(new File("D:\2_" + din.readUTF()));
+            byte[] inputByte = new byte[1024];
+            int length = 0;
+            while (true){
+                length = din.read(inputByte, 0, inputByte.length);
+                fos.write(inputByte, 0, length);
+                System.out.println(length + bytes downloaded.);
+                if (-1 == length)
+                {
+                    System.out.println("Finish to download file.");
+                    oos.writeByte(2);
+                    break;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Exception is thrown during download file.");
+        }finally {
+            try{
+                if (null != din) din.close();
+                if (null != fos) fos.close();
+            }catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Exception is thrown during clearing up DataInputStream and FileOutputStream.");
+            }
+        }
     }
 
     public void run() {
         try {
             if (null != socket) {
                 System.out.println(socket.getInetAddress() + ":" + socket.getPort() + " is connected");
-                //TODO:
+                bool flag = true;
+                while (true){
+                    int type = ois.readByte();
+                    if (1 == type)
+                    {
+                        DownloadFile();
+                    }
+                    else if(2 == type)
+                    {
+                        break;
+                    }
+                }
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
+                if (null != oos){
+                    oos.close();
+                }
+                if (null != ois){
+                    ois.close();
+                }
                 if (null != socket) {
                     System.out.println("Close connection " + socket.getInetAddress() + ":" + socket.getPort());
                     socket.close();
