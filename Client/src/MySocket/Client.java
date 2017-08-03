2af892810cec02d2422a4c80230e4df0351c356a
Andy Gong <gwqqw@163.com>
2017-08-03 22:48:07 +0800

    try single thread

--- Client/src/MySocket/Client.java
+++ Client/src/MySocket/Client.java
@@ -6,12 +6,12 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-class Handler implements Runnable{
+class ClientHandler implements Runnable{
     Socket socket;
     OutputStream out;
     InputStream in;
 
-    Handler(){
+    ClientHandler(){
         try {
 //            FileSelection fileSelection = new FileSelection();
 //            fileSelection.pack();
@@ -83,12 +83,55 @@ class Handler implements Runnable{
 
 public class Client {
     public static void main(String [] args){
+        Socket socket = null;
+        OutputStream out = null;
+        InputStream in = null;
 
+        JFileChooser fileChooser = null;
+        DataOutputStream dout = null;
+        DataInputStream din = null;
+        FileInputStream fin = null;
+        File file = null;
+        byte[] sendByte;
+        int length = 0;
         try{
-            //while (true){
-                Thread uploadThread = new Thread(new Handler());
-                uploadThread.start();
-            //}
+//            //while (true){
+////                Thread uploadThread = new Thread(new ClientHandler());
+////                uploadThread.start();
+//            //}
+
+            fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setAcceptAllFileFilterUsed(true);
+            fileChooser.setVisible(true);
+            fileChooser.showDialog(null, "Selection a file to upload");
+            String strFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(2055));
+            out = socket.getOutputStream();
+            //in = socket.getInputStream();
+
+            dout = new DataOutputStream(socket.getOutputStream());
+            //din = new DataInputStream(socket.getInputStream());
+            file = new File(strFilePath);
+            fin = new FileInputStream(file);
+            sendByte = new byte[1024];
+            System.out.println("Start to upload file...");
+            dout.writeByte(1);
+            while ((length = fin.read(sendByte, 0, sendByte.length)) > 0){
+                dout.write(sendByte,0, length);
+                dout.flush();
+            }
+            dout.writeByte(2);
+            while (true){
+                byte receive = din.readByte();
+                if (2 == receive)
+                {
+                    break;
+                }
+            }
+            System.out.println("Finish uploading file.");
 
         }catch (Exception e){
             e.printStackTrace();
--- src/MySocket/Server.java
+++ src/MySocket/Server.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-class Handler implements Runnable {
+class ServerHandler implements Runnable {
     private Socket socket;
     private InputStream in;
     private OutputStream out;
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
-    public Handler(ServerSocket serverSocket) {
+    public ServerHandler(ServerSocket serverSocket) {
         try {
             this.socket = serverSocket.accept();
             in = socket.getInputStream();
@@ -98,14 +98,13 @@ class Handler implements Runnable {
 
 public class Server {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         ServerSocket serverSocket = null;
         try {
-
             serverSocket = new ServerSocket(2055);
             while (true) {
                 try {
-                    Thread curConnection = new Thread((new Handler(serverSocket)));
+                    Thread curConnection = new Thread((new ServerHandler(serverSocket)));
                     curConnection.start();
 
                 } catch (Exception e) {
@@ -120,6 +119,76 @@ public class Server {
                 if (null != serverSocket) serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }*/
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        ObjectInputStream ois = null;
+        ObjectOutputStream oos = null;
+        try {
+            serverSocket = new ServerSocket(2055);
+            socket = serverSocket.accept();
+            InputStream in = socket.getInputStream();;
+            OutputStream out = socket.getOutputStream();
+            ois = new ObjectInputStream(in);
+            oos = new ObjectOutputStream(out);
+            byte flag = ois.readByte();
+            if (1 == flag)
+                DownloadFile(in, out, oos);
+            while (true){
+                flag = ois.readByte();
+                if (2 == flag)   break;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if (null != ois) ois.close();
+                if (null != oos) oos.close();
+                if (null != socket) socket.close();
+                if (null != serverSocket) serverSocket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void DownloadFile(InputStream in, OutputStream out, ObjectOutputStream oos )
+    {
+        DataInputStream din = null;
+        FileOutputStream fos = null;
+        try{
+            System.out.println("Start to download file...");
+            din = new DataInputStream(in);
+            fos = new FileOutputStream(new File("D:\2_" + din.readUTF()));
+            byte[] inputByte = new byte[1024];
+            int length = 0;
+            while (true){
+                length = din.read(inputByte, 0, inputByte.length);
+                fos.write(inputByte, 0, length);
+                fos.flush();
+                System.out.println(length + "bytes downloaded.");
+                if (-1 == length)
+                {
+                    System.out.println("Finish to download file.");
+                    oos.writeByte(2);
+                    oos.flush();
+                    break;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Exception is thrown during download file.");
+        }finally {
+            try{
+                if (null != din) din.close();
+                if (null != fos) fos.close();
+            }catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Exception is thrown during clearing up DataInputStream and FileOutputStream.");
             }
         }
     }
